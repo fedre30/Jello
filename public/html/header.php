ce57f8f50709343f11fe8fe0e8be51ad49b21fa2
mallano <maelallano1@gmail.com>
2018-02-12 13:17:33 +0100

    repo structure

---
+++ public/html/header.php
@@ -0,0 +1,13 @@
+<nav class="navBar">
+    <div class="navBar__logo">
+        <p>Jello</p>
+    </div>
+    <div class="navBar__research">
+        <label for=""></label> <input type="text" placeholder="...">
+        <img src="#" alt="A research icon">
+    </div>
+    <div class="navBar__account">
+        <p class="navBar__accountText">A</p>
+        <p class="navBar__accountLogOut">Log Out</p>
+    </div>
+</nav>
