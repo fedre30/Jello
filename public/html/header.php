8ce0e4ac3ffdf561db68cd775fdef3eb8fed69fb
mallano <maelallano1@gmail.com>
2018-02-12 17:38:26 +0100

    header.php

--- public/html/header.php
+++ public/html/header.php
@@ -1,13 +1,15 @@
 <nav class="navBar">
-    <div class="navBar__logo">
-        <p>Jello</p>
-    </div>
-    <div class="navBar__research">
-        <label for=""></label> <input type="text" placeholder="...">
-        <img src="#" alt="A research icon">
-    </div>
-    <div class="navBar__account">
-        <p class="navBar__accountText">A</p>
-        <p class="navBar__accountLogOut">Log Out</p>
+    <div class="navbar__Container">
+        <div class="navBar__logo">
+            <a class="navBar__logoLink" href="../../index.php">Jello</a>
+        </div>
+        <div class="navBar__research">
+            <label for=""></label> <input class="navBar__researchInputText" type="text" placeholder="">
+            <button class="navBar__researchBtn">research</button>
+        </div>
+        <div class="navBar__account">
+            <a href="account.php" class="navBar__accountText">My account</a>
+            <a href="login.php" class="navBar__accountLogOut">Log Out</a>
+        </div>
     </div>
 </nav>
