064ad5db9b13cbb4a2202d1a64e4942a806601d3
Geoffrey Girard <geoffreygirard28@gmail.com>
2018-02-13 10:09:29 +0100

    update

---
+++ public/html/board.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="../style/reset.css">
+  <link rel="stylesheet" href="../style/board.css">
+  <title>Document</title>
+</head>
+<body>
+
+  <div class="boardPage">
+
+    <!-- CARD AREA -->
+    <div class="cardArea">
+
+      <!-- TOOLS BAR -->
+      <div class="toolBar">
+        <h4 class="toolBar_title">SEMAINE INTENSIVE PHP</h4>
+      </div>
+
+      <!-- TAB -->
+      <div class="cardArea_container">
+        <h4 class="cardArea_container_title">TITLE TAB</h4>
+
+        <div class="cardArea_card">
+          <div class="cardArea_filter">color</div>
+          <p class="cardArea_title">TITLE CARD</p>
+        </div>
+
+        <div class="cardArea_card">
+          <div class="cardArea_filter">color</div>
+          <p class="cardArea_title">TITLE CARD</p>
+        </div>
+
+        <div class="cardArea_card">
+          <div class="cardArea_filter">color</div>
+          <p class="cardArea_title">TITLE CARD</p>
+        </div>
+
+        <img class="cardArea_addCard" src="../imgs/iconPlus.png" alt="add">
+
+      </div>
+
+      <div class="cardArea_container">
+        <h4 class="cardArea_container_title">TITLE TAB</h4>
+
+        <div class="cardArea_card">
+          <div class="cardArea_filter">color</div>
+          <p class="cardArea_title">TITLE CARD</p>
+        </div>
+
+        <div class="cardArea_card">
+          <div class="cardArea_filter">color</div>
+          <p class="cardArea_title">TITLE CARD</p>
+        </div>
+
+        <div class="cardArea_card">
+          <div class="cardArea_filter">color</div>
+          <p class="cardArea_title">TITLE CARD</p>
+        </div>
+
+        <img class="cardArea_addCard" src="../imgs/iconPlus.png" alt="addCard">
+
+      </div>
+
+      <div class="cardArea_addList">
+        <p>ADD LIST</p>
+        <img class="cardArea_addList_img" src="../imgs/iconPlus.png" alt="addList">
+      </div>
+
+
+    </div>
+  </div>
+
+</body>
+</html>
