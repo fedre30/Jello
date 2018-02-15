8765ebdbba0098e236f84109483abc423ead6ae7
Federica Alfano <alfanofederica95@gmail.com>
2018-02-15 21:53:02 +0100

    last changes

--- public/account.php
+++ public/account.php
@@ -20,13 +20,8 @@ $row = $db->getUserInformation($_GET['id']);
 <body>
 <a href="board.php?id=<?= $_GET['id'] ?>">Go back</a>
 
-<form action="../src/doEditUser.php" method="post">
-    <input type="hidden" name="userID" value="<?= $row['userID'] ?>">
-    <label for="lastName"></label> <input type="text" name="lastName" value="<?= $row['lastName'] ?>">
-    <label for="firstName"></label> <input type="text" name="firstName" value="<?= $row['firstName'] ?>">
-    <label for="email"></label> <input type="text" name="email" value="<?= $row['email'] ?>">
-    <label for="password"></label> <input type="password" name="password" value="<?= $row['password'] ?>">
-    <input type="submit" value="submit">
-</form>
+<p>First name: <?= $row['firstName'] ?></p>
+<p>Last name: <?= $row['lastName'] ?></p>
+<p>E-mail: <?= $row['email'] ?></p>
 </body>
 </html>
